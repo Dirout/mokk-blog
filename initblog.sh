c7995b035574d69a8fbbf9360990c1f84dd81dda
Emil Sayahi <32737166+limeschool@users.noreply.github.com>
2020-12-17 15:27:06 -0500

    Create initblog.sh

---
+++ initblog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+printf "Template Mokk GitHub repository: "
+read blogmokk
+printf "Basing blog off of https://github.com/${blogmokk}\n"
+mkdir ./blog
+cd ./blog
+git clone --depth 1 https://github.com/MadeByEmil/mokk-template.git --quiet
+git clone --depth 1 https://github.com/${blogmokk}.git --quiet
+cd ../
+if [ -f "./blog/setup.sh" ]; then
+    chmod +x ./blog/setup.sh && ./blog/setup.sh
+fi
