824d6565c84c770c9c8f9585881829d9a5831cb4
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2021-03-03 13:12:26 -0500

    Rename organization

--- initblog.sh
+++ initblog.sh
@@ -4,7 +4,7 @@ read blogmokk
 printf "Basing blog off of https://github.com/${blogmokk}\n"
 mkdir ./blog
 cd ./blog
-git clone --depth 1 https://github.com/MadeByEmil/mokk-template.git --quiet
+git clone --depth 1 https://github.com/Dirout/mokk-template.git --quiet
 git clone --depth 1 https://github.com/${blogmokk}.git --quiet
 cd ../
 if [ -f "./blog/setup.sh" ]; then
